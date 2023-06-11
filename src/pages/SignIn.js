59b8e5c171dfe546490e7375c891b3721caa1d61
Reggy991 <reggyvliempd@gmail.com>
2023-06-11 22:02:52 +0200

    Finished homework Banana Security for this week, except for bonus assignment 2

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from "../context/AuthContext";
 
 function SignIn() {
-  return (
-    <>
-      <h1>Inloggen</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias qui quo unde?</p>
+    const { isAuth, login, logout } = useContext(AuthContext);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-      <form>
-        <p>*invoervelden*</p>
-        <button>Inloggen</button>
-      </form>
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
 
-      <p>Heb je nog geen account? <Link to="/signup">Registreer</Link> je dan eerst.</p>
-    </>
-  );
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Communiceer hier wat je met de gegevens wil doen.
+        login(email, password);
+    };
+
+    return (
+        <>
+            <h1>Inloggen</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias qui quo unde?</p>
+
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Emailadres:</label>
+                    <input type="email" value={email} onChange={handleEmailChange} required />
+                </div>
+                <div>
+                    <label>Wachtwoord:</label>
+                    <input type="password" value={password} onChange={handlePasswordChange} required />
+                </div>
+                <button type="submit">Inloggen</button>
+            </form>
+
+            <p>Heb je nog geen account? <Link to="/signup">Registreer</Link> je dan eerst.</p>
+        </>
+    );
 }
 
 export default SignIn;
